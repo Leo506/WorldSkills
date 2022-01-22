1d70e99742d5e94c7bb9834766f4fa734f6d69d4
Artem <leoartemlev@yandex.ru>
2022-01-23 01:50:39 +0500

    Tilemap simple generation

---
+++ Assets/Scripts/TilemapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapGenerator : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Tile[] tiles;
+
+    private void Start()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                var xPos = (int)(x - y / 2 + 0.5f * y) - 5;
+                var yPos = (int)y - 5;
+
+                tilemap.SetTile(new Vector3Int(xPos, yPos, 0), tiles[Random.Range(0, tiles.Length)]);
+            }
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var tile = tilemap.WorldToCell(origin);
+            Debug.Log($"WorldPos: {origin} GetCellCenterWorld: {tilemap.GetCellCenterWorld(tile)}");
+    
+        }
+    }
+}
